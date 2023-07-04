c7c91f8a665e5c5ca897a5cdbb6eedfabe2575ac
Henrik Santos <henrik.ruan4@gmail.com>
2023-07-03 22:40:31 -0300

    deve falhar

--- __tests__/pages/page-01/page-01.test.tsx
+++ __tests__/pages/page-01/page-01.test.tsx
@@ -3,12 +3,12 @@ import Page01 from '@/pages/page-01'
 
 describe('Page01', () => {
   it('renders a page', () => {
-    render(<Page01 />)
+    // render(<Page01 />)
 
-    const heading = screen.getByRole('heading', {
-      name: /this is the page 01/i
-    })
+    // const heading = screen.getByRole('heading', {
+    //   name: /this is the page 01/i
+    // })
 
-    expect(heading).toBeInTheDocument()
+    // expect(heading).toBeInTheDocument()
   })
 })
