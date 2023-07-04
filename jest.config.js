d64d57c9c51071e04b72f0c90efb076f14f9ac24
Henrik Santos <henrik.ruan4@gmail.com>
2023-07-03 22:05:05 -0300

    remove test coverage in normal test

--- jest.config.js
+++ jest.config.js
@@ -18,15 +18,7 @@ const customJestConfig = {
     "/node_modules/",
     "_app.tsx"
   ],
-  collectCoverage: true,
-  coverageThreshold: {
-    "global": {
-      "branches": 80,
-      "functions": 80,
-      "lines": 80,
-      "statements": 80
-    }
-  }
+  collectCoverage: true
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
