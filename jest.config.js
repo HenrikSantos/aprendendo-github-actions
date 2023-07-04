c4118033920e89c57128d8cbf8312cef98588bdc
Henrik Santos <henrik.ruan4@gmail.com>
2023-07-03 22:01:34 -0300

    update: raise an error if coverage is lower than 80%

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,16 @@ const customJestConfig = {
   coveragePathIgnorePatterns: [
     "/node_modules/",
     "_app.tsx"
-  ]
+  ],
+  collectCoverage: true,
+  coverageThreshold: {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  }
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
